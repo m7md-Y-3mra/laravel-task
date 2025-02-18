2377872c196e73e9721d53ee4e907ef4f49d50e9
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-02-18 12:03:02 +0200

    HW3: add delete, update and edit operation

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -20,6 +21,7 @@
         }
     </style>
 </head>
+
 <body id="app-layout">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
@@ -31,29 +33,57 @@
 
     <div class="container mt-4">
         <div class="offset-md-2 col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    New Task
-                </div>
-                <div class="card-body">
-                    <!-- New Task Form -->
-                    <form action="create" method="POST">
-                        @csrf
-                        <!-- Task Name -->
-                        <div class="mb-3">
-                            <label for="task-name" class="form-label">Task</label>
-                            <input type="text" name="name" id="task-name" class="form-control" value="">
-                        </div>
+            @if (isset($task))
+                <div class="card">
+                    <div class="card-header">
+                        update Task
+                    </div>
+                    <div class="card-body">
+                        <!-- New Task Form -->
+                        <form action="{{ url('update') }}" method="POST">
+                            @csrf
+                            <input type="hidden" name="id" value="{{ $task->id }}">
+                            <!-- Task Name -->
+                            <div class="mb-3">
+                                <label for="task-name" class="form-label">Task</label>
+                                <input type="text" name="name" id="task-name" class="form-control"
+                                    value="{{ $task->name }}">
+                            </div>
 
-                        <!-- Add Task Button -->
-                        <div>
-                            <button type="submit" class="btn btn-primary">
-                                <i class="fa fa-plus me-2"></i>Add Task
-                            </button>
-                        </div>
-                    </form>
+                            <!-- Add Task Button -->
+                            <div>
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-plus me-2"></i>Update Task
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            @else
+                <div class="card">
+                    <div class="card-header">
+                        New Task
+                    </div>
+                    <div class="card-body">
+                        <!-- New Task Form -->
+                        <form action="create" method="POST">
+                            @csrf
+                            <!-- Task Name -->
+                            <div class="mb-3">
+                                <label for="task-name" class="form-label">Task</label>
+                                <input type="text" name="name" id="task-name" class="form-control" value="">
+                            </div>
+
+                            <!-- Add Task Button -->
+                            <div>
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-plus me-2"></i>Add Task
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            @endif
 
             <!-- Current Tasks -->
             <div class="card mt-4">
@@ -69,36 +99,27 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Task 1</td>
-                                <td>
-                                    <form action="#" method="POST" class="d-inline">
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash me-2"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Task 2</td>
-                                <td>
-                                    <form action="#" method="POST" class="d-inline">
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash me-2"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Task 3</td>
-                                <td>
-                                    <form action="#" method="POST" class="d-inline">
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash me-2"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
+                            @foreach ($tasks as $task)
+                                <tr>
+                                    <td> {{ $task->name }} </td>
+                                    <td>
+                                        <form action="delete/{{ $task->id }}" method="POST" class="d-inline">
+
+                                            @csrf
+                                            <button type="submit" class="btn btn-danger">
+                                                <i class="fa fa-trash me-2"></i>Delete
+                                            </button>
+                                        </form>
+                                        <form action="edit/{{ $task->id }}" method="POST" class="d-inline">
+                                            @csrf
+                                            <button type="submit" class="btn btn-info">
+                                                <i class="fa fa-info me-2"></i>Edit
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+
                         </tbody>
                     </table>
                 </div>
@@ -109,4 +130,5 @@
     <!-- JavaScripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,33 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Route::get('tasks', function () {
-    return view('tasks');
+  $tasks = DB::table('tasks')->get();
+  return view('tasks', compact('tasks'));
 });
 
 Route::post('create', function () {
-    $name = $_POST['name'];
-    DB::table('tasks')->insert(['name' => $name ]);
-    return view('tasks');
+  $name = $_POST['name'];
+  DB::table('tasks')->insert(['name' => $name]);
+  return redirect()->back();
+});
+
+Route::post('delete/{id}' , function($id){
+    DB::table('tasks')->where('id' , $id)->delete();
+    return redirect()->back();
+});
+
+Route::post('edit/{id}' , function ($id){
+    $task = DB::table('tasks')->where('id' , $id)->first();
+    $tasks = DB::table('tasks')->get();
+    return view('tasks' , compact('task' , 'tasks'));
+});
+
+Route::post('update', function () {
+    $id = $_POST['id'];
+    DB::table('tasks')->where('id', $id)->update(['name' => $_POST['name'] ]);
+    return redirect('tasks');
 });

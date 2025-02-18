e536cdf21803d1427744c6d0bfda3905f1e37ff7
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-02-18 11:44:55 +0200

    HW2: add tasks table and store the values in DB

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('tasks', function () {
+    return view('tasks');
+});
+
+Route::post('create', function () {
+    $name = $_POST['name'];
+    DB::table('tasks')->insert(['name' => $name ]);
+    return view('tasks');
 });
